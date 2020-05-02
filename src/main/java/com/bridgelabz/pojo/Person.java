886739c880386d3e678ed467c0b83048b27c5ce7
vampire16 <hakevaibhav16@gmail.com>
2020-05-02 07:06:51 +0530

    ADDED : Pojo class of person

--- src/main/java/com/bridgelabz/pojo/Person.java
+++ src/main/java/com/bridgelabz/pojo/Person.java
@@ -1,4 +1,80 @@
 package com.bridgelabz.pojo;
 
 public class Person {
+    String firstName;
+    String lastName;
+    String address;
+    String city;
+    String state;
+    String zip;
+    String phone;
+
+    public Person() {
+    }
+
+    public Person(String firstName, String lastName, String address, String city, String state, String zip, String phone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phone = phone;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
