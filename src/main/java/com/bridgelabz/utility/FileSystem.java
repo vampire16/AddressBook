e151b2360c604a9d25fcddde92b20cfbb1ededc4
vampire16 <hakevaibhav16@gmail.com>
2020-05-02 07:08:21 +0530

    ADDED : FileSystem class for to make file

--- src/main/java/com/bridgelabz/utility/FileSystem.java
+++ src/main/java/com/bridgelabz/utility/FileSystem.java
@@ -1,4 +1,17 @@
 package com.bridgelabz.utility;
 
+import java.io.File;
+
 public class FileSystem {
+    static File file;
+    String PATH = "src/test/resources/";
+
+
+    public FileSystem(String fileName) {
+        file = new File(PATH + fileName + ".json");
+    }
+
+    public static File getFile(){
+        return file;
+    }
 }
