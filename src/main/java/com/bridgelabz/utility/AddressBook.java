502f1b0e4cefebd7c1d1d357bea9981991191406
vampire16 <hakevaibhav16@gmail.com>
2020-05-02 07:07:32 +0530

    ADDED : Abstract class

--- src/main/java/com/bridgelabz/utility/AddressBook.java
+++ src/main/java/com/bridgelabz/utility/AddressBook.java
@@ -1,4 +1,28 @@
 package com.bridgelabz.utility;
 
-public class AddressBook {
+import com.bridgelabz.pojo.Person;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class AddressBook {
+    public abstract List<Person> addPerson(Person person) throws IOException;
+
+    public abstract  int getCountOfRecords() throws IOException;
+
+    public abstract Person getPersonInfo(int index) throws IOException;
+
+    public abstract ArrayList<Person> updatePerson(int index, Person person) throws IOException;
+
+    public abstract int removePerson(int index) throws IOException;
+
+    public abstract ArrayList<Person> sortByName() throws IOException;
+
+    public abstract ArrayList<Person> sortByZip() throws IOException;
+
+    public abstract File getFile();
+
+    public abstract void printAll() throws IOException;
 }
