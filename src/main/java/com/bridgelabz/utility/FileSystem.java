1e3d4b1e221cf9e357212b40ae84795f5a8619c3
vampire16 <hakevaibhav16@gmail.com>
2020-05-04 18:57:16 +0530

    REFACTORED : Indentation

--- src/main/java/com/bridgelabz/utility/FileSystem.java
+++ src/main/java/com/bridgelabz/utility/FileSystem.java
@@ -3,15 +3,15 @@ package com.bridgelabz.utility;
 import java.io.File;
 
 public class FileSystem {
+    public static String PATH = "src/test/resources/";
     static File file;
-    String PATH = "src/test/resources/";
 
 
     public FileSystem(String fileName) {
         file = new File(PATH + fileName + ".json");
     }
 
-    public static File getFile(){
+    public static File getFile() {
         return file;
     }
 }
